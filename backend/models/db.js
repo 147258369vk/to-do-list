4b2e4bbd43b45ee79ac83f63ebb3f2910ee13240
Robin Singh <robinsinghkamboj@gmail.com>
2019-11-29 13:22:11 +0530

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -10,8 +10,8 @@
 // })
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+// const uri = "mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority";
+const client = new MongoClient('mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority', { useNewUrlParser: true });
 client.connect(err => {
   //const collection = client.db("test").collection("devices");
   // perform actions on the collection object
