223ca388ff9ffa43c217c0736f0c2188da1ed40c
Robin Singh <robinsinghkamboj@gmail.com>
2019-11-29 13:35:51 +0530

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,19 +1,19 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority', { useNewUrlParser: true}, (err) => {
-//     if(!err) {
-//         console.log('MongoDB connection succeeded!!!')
-//     }
-//     else {
-//         console.log('Error in DB connection: ' + err)
-//     }
-// })
+mongoose.connect('mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority&connectTimeoutMS=50000&bufferCommands=false', { useNewUrlParser: true,keepAlive: true, keepAliveInitialDelay: 300000,urlUnifiedTopology:true}, (err) => {
+    if(!err) {
+        console.log('MongoDB connection succeeded!!!')
+    }
+    else {
+        console.log('Error in DB connection: ' + err)
+    }
+})
 
-const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority";
-const client = new MongoClient('mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority', { useNewUrlParser: true });
-client.connect(err => {
-  //const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// // const uri = "mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority";
+// const client = new MongoClient('mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority', { useNewUrlParser: true });
+// client.connect(err => {
+//   //const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
