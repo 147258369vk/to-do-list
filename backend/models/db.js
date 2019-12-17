e8d04ecc2407799f7a90c6a1c8fdead63f7a8f4d
147258369vk <45756619+147258369vk@users.noreply.github.com>
2019-12-17 11:55:50 +0530

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list?retryWrites=true&w=majority&connectTimeoutMS=500000&bufferCommands=false', { useNewUrlParser: true,keepAlive: true, keepAliveInitialDelay: 300000,useUnifiedTopology:true}, (err) => {
+mongoose.connect('mongodb+srv://robinsinghkamboj:robinsingh@cluster0-rneh1.gcp.mongodb.net/to-do-list, { useNewUrlParser: true,keepAlive: true, keepAliveInitialDelay: 300000,useUnifiedTopology:true}, (err) => {
     if(!err) {
         console.log('MongoDB connection succeeded!!!')
     }
